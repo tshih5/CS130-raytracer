04e937f2dd79f6ecceb907e7297e4b04bc256ce3
Tom Shih <tshih001@bolt.cs.ucr.edu>
2020-01-22 19:25:34 -0800

    added changes for lab 2

--- proj-rt-files/grading/sphere.cpp
+++ proj-rt-files/grading/sphere.cpp
@@ -5,25 +5,26 @@
 Hit Sphere::Intersection(const Ray& ray, int part) const
 {
     TODO;
-    //L is the vector from the center of the sphere to the endpoint of the ray
-	vec3 L = this->center - ray.endpoint;
-	double mag_L = L.magnitude();
-	//tc is the magnitude of the projection of L onto the ray
-	double tc = dot(L, ray.direction);
-	if(tc < small_t){
-		return {NULL, 0, 0};
+	double a = dot(ray.direction, ray.direction);
+	double b = 2 * dot(ray.direction, (ray.endpoint - this->center));
+	double c = dot(ray.endpoint - this->center, ray.endpoint - this->center) - (this->radius * this->radius);
+	double discrim = (b * b) - (4 * a * c);
+	if(discrim < 0){
+			return {NULL, 0, 0};
 	}
-	//d is the distance between the center of the sphere and avector perpendicular 
-	//to the ray that touches the center of the sphere
 	
-	double d = sqrt((tc * tc) - (mag_L * mag_L));
-	if(d > this->radius){
-		return {NULL, 0, 0};
-	}	
-	double t1c = sqrt((this->radius * this-> radius) - (d * d));
-	double t1 = tc - t1c;
-	
-    return {this, t1, part};
+	double t1 = (-b + sqrt(discrim)) / (2 * a);
+	double t2 = (-b - sqrt(discrim)) / (2 * a);
+	if(t1 >= small_t || t2 >= small_t ){
+		if(t2 < t1 && t2 >= small_t){
+			return {this, t2, part};
+		}else if(t1 < t2 && t2 >= small_t){
+			return {this, t1, part};
+		}
+
+	}else{
+			return {NULL, 0, 0};
+	}
  
 }
 
